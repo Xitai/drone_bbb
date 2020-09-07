dec092a41bdd111ed9950aeb36a552c600ae4b0f
Xitai <huvluv14@gmail.com>
2020-09-08 07:37:37 +0900

    2020-09-08,Tue,awt
    prj: example_csl_epwm_duty_cycle_test,
    issue: epwm not working
    check: reference register

---
+++ example_csl_epwm_duty_cycle_test/platform/bbbAM335x_drone/package/package_bbbAM335x_drone.c
@@ -0,0 +1,21 @@
+/*
+ *  Do not modify this file; it is automatically 
+ *  generated and any modifications will be overwritten.
+ *
+ * @(#) xdc-I11
+ */
+
+#include <xdc/std.h>
+
+__FAR__ char bbbAM335x_drone__dummy__;
+
+#define __xdc_PKGVERS null
+#define __xdc_PKGNAME bbbAM335x_drone
+#define __xdc_PKGPREFIX bbbAM335x_drone_
+
+#ifdef __xdc_bld_pkg_c__
+#define __stringify(a) #a
+#define __local_include(a) __stringify(a)
+#include __local_include(__xdc_bld_pkg_c__)
+#endif
+
