acfc9b1db9b026a2fb521f688d3ecf58e6f3c82e
Xitai <huvluv14@gmail.com>
2020-09-25 08:52:59 +0900

    ECU Code Added

--- example_sysbios_starterware_epwm/Code/epwm_app.h
+++ example_sysbios_starterware_epwm/Code/epwm_app.h
@@ -1,42 +1,6 @@
 /**
  *  \file  epwm_app.h
  *
- *  \brief This file contains the function declarations and data structures
- *         corresponding to the epwm IP configuration.
- *
- *  \copyright Copyright (C) 2013 Texas Instruments Incorporated -
- *             http://www.ti.com/
- */
-
-/*
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *    Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- *    Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the
- *    distribution.
- *
- *    Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
  */
 
 #ifndef EPWM_APP_H_
@@ -55,7 +19,6 @@ extern "C" {
 /* ========================================================================== */
 
 /* None */
-
 
 /* ========================================================================== */
 /*                         Structures and Enums                               */
@@ -113,22 +76,13 @@ typedef struct epwmAppPwmObj
 /*                         Global Variables Declarations                      */
 /* ========================================================================== */
 
-extern epwmAppPwmObj_t CONF_EPWM1_MOTOR;
-//extern epwmAppPwmObj_t CONF_EPWM1B_MOTOR;
+/* None */
 
 /* ========================================================================== */
 /*                          Function Declarations                             */
 /* ========================================================================== */
-/**
- * \brief   EPWM IP configuration functional API.
- *
- * \param   pPwm        Pointer to the pwm object.
- *
- * \retval  S_PASS      IP configuration is successful.
- * \retval  E_FAIL      IP configuration is failed(either Pinmux or Clock
- *                      configuration is failed) is failed.
- */
-uint32_t EPWMAppInit(epwmAppPwmObj_t *pPwm);
+
+int32_t InitECU(void);
 
 #ifdef __cplusplus
 }
--- example_sysbios_starterware_epwm/Code/main.c
+++ example_sysbios_starterware_epwm/Code/main.c
@@ -27,24 +27,24 @@
 #include <ti/drv/uart/test/src/UART_board.h>
 
 ///* I2C header files */
-#include <ti/drv/i2c/I2C.h>
-#include <ti/drv/i2c/soc/I2C_soc.h>
+//#include <ti/drv/i2c/I2C.h>
+//#include <ti/drv/i2c/soc/I2C_soc.h>
 //#include <ti/csl/soc.h>
-#include <ti/starterware/include/hw/soc_am335x.h>
+//#include <ti/starterware/include/hw/soc_am335x.h>
 
 /* ========================================================================== */
 /*                             Include Files                                  */
 /* ========================================================================== */
-#include "types.h"
-#include "error.h"
-#include "board.h"
-#include "soc.h"
-#include "device.h"
-#include "chipdb.h"
-#include "cache.h"
-#include "mmu.h"
-#include "example_utils_mmu.h"
-#include "console_utils.h"
+//#include "types.h"
+//#include "error.h"
+//#include "board.h"
+//#include "soc.h"
+//#include "device.h"
+//#include "chipdb.h"
+//#include "cache.h"
+//#include "mmu.h"
+//#include "example_utils_mmu.h"
+//#include "console_utils.h"
 
 /* Application header files */
 #include "epwm_app.h"
@@ -64,22 +64,13 @@
 /*                 Internal Function Declarations                             */
 /* ========================================================================== */
 
-/**
- * \brief   This API gets the soc info - base address of epwm instance.
- *
- * \param   pObj     Pointer to the pwm object.
- *
- * \retval  S_PASS   SOC info is successfully retrieved.
- * \retval  E_FAIL   SOC info retrieval is failed. Resource is not present.
- */
-static uint32_t EpwmAppSocInfoGet(epwmAppPwmObj_t *pObj);
+/* None */
 
 /* ========================================================================== */
 /*                            Global Variables                                */
 /* ========================================================================== */
 
-/** \brief Global object for the EPWM IP configuration data structure.  */
-static epwmAppPwmObj_t gPwmCfg;
+/* None */
 
 /* ========================================================================== */
 /*                          Function Definitions                              */
@@ -113,6 +104,8 @@ bool initBbbDevice(void)
  */
 Void taskFxn(UArg a0, UArg a1)
 {
+	int32_t status = S_PASS;
+	
     System_printf("enter taskFxn()\n");
 
 	initBbbDevice();
@@ -123,37 +116,18 @@ Void taskFxn(UArg a0, UArg a1)
 	 * 						START EPWM
 	 */
 	
-    int32_t status = S_PASS;
-
-
-    /* Initialize the global IP configuration with default configuration. */
-    gPwmCfg     = CONF_EPWM1_MOTOR;
-
-    UART_printf("\n StarterWare EPWM Application!!\n");
-    UART_printf("BOARDInit status [0x%x]\n", status);
-
-	/* Get SoC info */
-	status = EpwmAppSocInfoGet(&gPwmCfg);
+	status = InitECU();
 	if(S_PASS == status)
 	{
-		/* EPWM IP configuration */
-		status = EPWMAppInit(&gPwmCfg);
-		if(S_PASS == status)
+		UART_printf("\nNow Initializing ECU ...\n");
+		while(1)
 		{
-			UART_printf("\nNow running the haptics motor ...\n");
-			while(1)
-			{
-				/* Infinite loop */
-			}
-		}
-		else
-		{
-			UART_printf("\n FAILURE!!! EPWM IP configuration is failed ...\n");
+			/* Infinite loop */
 		}
 	}
 	else
 	{
-		UART_printf("\nEPWM IP instance is not present ...\n");
+		UART_printf("\nECU Initializing Fail...\n");
 	}
 
     System_printf("exit taskFxn()\n");
@@ -187,22 +161,22 @@ Int main()
 /* -------------------------------------------------------------------------- */
 /*                 Internal Function Definitions                              */
 /* -------------------------------------------------------------------------- */
-
-static uint32_t EpwmAppSocInfoGet(epwmAppPwmObj_t *pObj)
-{
-    uint32_t status = S_PASS;
-
-    /* Get the base address of the Module */
-    if(TRUE == CHIPDBIsResourcePresent(CHIPDB_MOD_ID_PWMSS, pObj->instNum))
-    {
-        pObj->instAddr = CHIPDBBaseAddress(CHIPDB_MOD_ID_PWMSS, pObj->instNum);
-    }
-    else
-    {
-       UART_printf("PWMSS instance number is not present!\n");
-       status = E_FAIL;
-    }
-
-    return status;
-}
+//
+//static uint32_t EpwmAppSocInfoGet(epwmAppPwmObj_t *pObj)
+//{
+//    uint32_t status = S_PASS;
+//
+//    /* Get the base address of the Module */
+//    if(TRUE == CHIPDBIsResourcePresent(CHIPDB_MOD_ID_PWMSS, pObj->instNum))
+//    {
+//        pObj->instAddr = CHIPDBBaseAddress(CHIPDB_MOD_ID_PWMSS, pObj->instNum);
+//    }
+//    else
+//    {
+//       UART_printf("PWMSS instance number is not present!\n");
+//       status = E_FAIL;
+//    }
+//
+//    return status;
+//}
 
